97aed366b26a2929cd5b33b1825673a91f36f868
Maxim Rozhkov <rojkov.m@rambler.ru>
2022-10-17 12:04:55 +0300

    init

---
+++ UIKitInstagram/UIKitInstagram/SceneDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  SceneDelegate.swift
+//  UIKitInstagram
+//
+//  Created by Maxim on 17.10.2022.
+//
+
+import UIKit
+///  Начальный SceneDelegate
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession,
+               options connectionOptions: UIScene.ConnectionOptions) {
+    }
+}
---
+++ UIKitInstagram/UIKitInstagram/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  UIKitInstagram
+//
+//  Created by Maxim on 17.10.2022.
+//
+
+import UIKit
+/// Начальный ViewController
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
